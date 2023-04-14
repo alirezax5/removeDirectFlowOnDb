bce2aa355c2d583d3437f4b84def4b05db251aa3
alirezax5 <alirezaaghax6@gmail.com>
2023-04-14 16:20:11 +0330

    update xtls to vision

--- script.sh
+++ script.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 sqlite3 x-ui.db <<EOF
+UPDATE inbounds SET settings = REPLACE(settings, 'xtls-rprx-direct', 'xtls-rprx-vision')  where stream_settings like '%xtls%'
+UPDATE inbounds SET stream_settings = REPLACE(settings, 'xtls', 'tls')  where stream_settings like '%xtls%'
 UPDATE inbounds SET settings = REPLACE(settings, '"flow": "xtls-rprx-direct",', '')
+
 EOF
