4d53b80b023103ebc1a1b37bab3e0703d6e7af7a
alirezax5 <alirezaaghax6@gmail.com>
2023-04-14 16:24:23 +0330

    fix

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 sqlite3 x-ui.db <<EOF
 UPDATE inbounds SET settings = REPLACE(settings, 'xtls-rprx-direct', 'xtls-rprx-vision')  where stream_settings like '%xtls%'
 UPDATE inbounds SET stream_settings = REPLACE(settings, 'xtls', 'tls')  where stream_settings like '%xtls%'
-UPDATE inbounds SET settings = REPLACE(settings, '"flow": "xtls-rprx-direct",', '')
+UPDATE inbounds SET settings = REPLACE(settings, '"flow": "xtls-rprx-direct",', '') where protocol= 'trojan'
 
 EOF
